a4ba6d1990462bc91bd94f7ea9dde87354f6f73d
Cédric Dujardin <51912624+cdujardin4000@users.noreply.github.com>
2022-11-07 23:28:19 +0100

    files

--- src/DataFixtures/MovieFixtures.php
+++ src/DataFixtures/MovieFixtures.php
@@ -9,6 +9,7 @@ use App\Entity\Actor;
 use App\Entity\Category;
 use App\Repository\MovieRepository;
 use Doctrine\Common\DataFixtures\DependentFixtureInterface;
+use DateTime;
 
 class MovieFixtures extends Fixture implements DependentFixtureInterface
 {
@@ -17,7 +18,7 @@ class MovieFixtures extends Fixture implements DependentFixtureInterface
         $movies = [
             [
                 'title' => 'Eternal Sunchine Of The Spotless Mind',
-                'description' => `L'idylle entre Clementine et Joel a pris fin, en raison de leurs caractères trop différents et de la routine. Pour apaiser ses souffrances, Clementine a recours à Lacuna, un procédé révolutionnaire qui efface certains souvenirs. Désespéré, Joel décide de `,
+                'description' => "L'idylle entre Clementine et Joel a pris fin, en raison de leurs caractères trop différents et de la routine. Pour apaiser ses souffrances, Clementine a recours à Lacuna, un procédé révolutionnaire qui efface certains souvenirs. Désespéré, Joel décide de ",
                 'category' => 'drame',
                 'releaseDate' => '2004-10-12',
                 'actors' => [
@@ -26,8 +27,8 @@ class MovieFixtures extends Fixture implements DependentFixtureInterface
                 ]
             ],
             [
-                'title' => `Vol au-dessus d'un nid de coucou`,
-                'description' => `Pour échapper à la prison, Randall P. McMurphy se fait volontairement interner dans une clinique psychiatrique. Il y découvre injustice et oppression`,
+                'title' => "Vol au-dessus d'un nid de coucou",
+                'description' => 'Pour échapper à la prison, Randall P. McMurphy se fait volontairement interner dans une clinique psychiatrique. Il y découvre injustice et oppression',
                 'category' => 'drame',
                 'releaseDate' => '1976-12-01',
                 'actors' => [
@@ -36,8 +37,8 @@ class MovieFixtures extends Fixture implements DependentFixtureInterface
                 ]
             ],
             [
-                'title' => `Requiem for a Dream`,
-                'description' => `Pour échapper à la prison, Randall P. McMurphy se fait volontairement interner dans une clinique psychiatrique. Il y découvre injustice et oppression`,
+                'title' => 'Requiem for a Dream',
+                'description' => 'Pour échapper à la prison, Randall P. McMurphy se fait volontairement interner dans une clinique psychiatrique. Il y découvre injustice et oppression',
                 'category' => 'drame',
                 'releaseDate' => '2001-03-21',
                 'actors' => [
@@ -46,8 +47,8 @@ class MovieFixtures extends Fixture implements DependentFixtureInterface
                 ]
             ],
             [
-                'title' => `De battre mon coeur s'est arreté`,
-                'description' => `A 28 ans, Tom semble marcher sur les traces de son père dans l'immobilier véreux. Mais une rencontre fortuite le pousse à croire qu'il pourrait être le pianiste concertiste de talent qu'il rêvait de devenir, à l'image de sa mère. Sans cesser ses activités`,
+                'title' => "De battre mon coeur s'est arreté",
+                'description' => "A 28 ans, Tom semble marcher sur les traces de son père dans l'immobilier véreux. Mais une rencontre fortuite le pousse à croire qu'il pourrait être le pianiste concertiste de talent qu'il rêvait de devenir, à l'image de sa mère. Sans cesser ses activités",
                 'category' => 'drame',
                 'releaseDate' => '2005-03-16',
                 'actors' => [
@@ -56,8 +57,8 @@ class MovieFixtures extends Fixture implements DependentFixtureInterface
                 ]
             ],
             [
-                'title' => `The Big Lebowski`,
-                'description' => `À Los Angeles, Jeffrey Lebowski, alias le Dude, mène une vie nonchalante. Il passe le plus clair de son temps à ne rien faire en robe de chambre ou à jouer au bowling du coin avec ses copains Walter, un vétéran du Vietnam, et Donny. Un jour, alors qu'il..`,
+                'title' => 'The Big Lebowski',
+                'description' => "À Los Angeles, Jeffrey Lebowski, alias le Dude, mène une vie nonchalante. Il passe le plus clair de son temps à ne rien faire en robe de chambre ou à jouer au bowling du coin avec ses copains Walter, un vétéran du Vietnam, et Donny. Un jour, alors qu'il..",
                 'category' => 'comédie',
                 'releaseDate' => '1998-04-22',
                 'actors' => [
@@ -66,8 +67,8 @@ class MovieFixtures extends Fixture implements DependentFixtureInterface
                 ]
             ],
             [
-                'title' => `Fight Club`,
-                'description' => `Insomniaque, désillusionné par sa vie personnelle et professionnelle, un jeune cadre expert en assurances, mène une vie monotone et sans saveur. Face à la vacuité de son existence, son médecin lui conseille de suivre une thérapie afin de relativiser son..`,
+                'title' => 'Fight Club',
+                'description' => 'Insomniaque, désillusionné par sa vie personnelle et professionnelle, un jeune cadre expert en assurances, mène une vie monotone et sans saveur. Face à la vacuité de son existence, son médecin lui conseille de suivre une thérapie afin de relativiser son..',
                 'category' => 'comédie',
                 'releaseDate' => '1999-11-10',
                 'actors' => [
@@ -76,8 +77,8 @@ class MovieFixtures extends Fixture implements DependentFixtureInterface
                 ]
             ],
             [
-                'title' => `The Butterfly Effect`,
-                'description' => `Evan Treborn a le pouvoir de remonter le temps pour corriger ses erreurs. C'est sans compter sur l'effet papillon qui engendre des déconvenues non prévues.`,
+                'title' => 'The Butterfly Effect',
+                'description' => "Evan Treborn a le pouvoir de remonter le temps pour corriger ses erreurs. C'est sans compter sur l'effet papillon qui engendre des déconvenues non prévues.",
                 'category' => 'science-fiction',
                 'releaseDate' => '2004-03-10',
                 'actors' => [
@@ -91,11 +92,12 @@ class MovieFixtures extends Fixture implements DependentFixtureInterface
             $m = new Movie();
             $m->setTitle($movie['title']);
             $m->setDescription($movie['description']);
-            $m->setReleaseDate($movie['releaseDate']);
+            $m->setReleaseDate(new DateTime($movie['releaseDate']));
             $m->setCategory($this->getReference($movie['category']));
 
             foreach($movie['actors'] as $actor) {
-                $m->addActor($this->getReference($actor));
+                $n =  explode(' ',$actor);
+                $m->addActor($this->getReference($actor[$n[1]]));
             }
             $this-> addReference($movie['title'],$m);
 
@@ -108,8 +110,9 @@ class MovieFixtures extends Fixture implements DependentFixtureInterface
     public function getDependencies()
     {
         return [
-            CategoryFixtures::class,
             ActorFixtures::class,
+            CategoryFixtures::class,
+
         ];
     }
 }
