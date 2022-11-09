36f5d6fbb3a8a9e8400a481033d045cc027941a7
Cédric Dujardin <51912624+cdujardin4000@users.noreply.github.com>
2022-11-09 19:44:24 +0100

    files

---
+++ src/Controller/ActorController.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Actor;
+use App\Form\ActorType;
+use App\Repository\ActorRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/actor')]
+class ActorController extends AbstractController
+{
+    #[Route('/', name: 'app_actor_index', methods: ['GET'])]
+    public function index(ActorRepository $actorRepository): Response
+    {
+        return $this->render('actor/index.html.twig', [
+            'actors' => $actorRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/new', name: 'app_actor_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, ActorRepository $actorRepository): Response
+    {
+        $actor = new Actor();
+        $form = $this->createForm(ActorType::class, $actor);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $actorRepository->save($actor, true);
+
+            return $this->redirectToRoute('app_actor_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('actor/new.html.twig', [
+            'actor' => $actor,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_actor_show', methods: ['GET'])]
+    public function show(Actor $actor): Response
+    {
+        return $this->render('actor/show.html.twig', [
+            'actor' => $actor,
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'app_actor_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Actor $actor, ActorRepository $actorRepository): Response
+    {
+        $form = $this->createForm(ActorType::class, $actor);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $actorRepository->save($actor, true);
+
+            return $this->redirectToRoute('app_actor_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('actor/edit.html.twig', [
+            'actor' => $actor,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_actor_delete', methods: ['POST'])]
+    public function delete(Request $request, Actor $actor, ActorRepository $actorRepository): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$actor->getId(), $request->request->get('_token'))) {
+            $actorRepository->remove($actor, true);
+        }
+
+        return $this->redirectToRoute('app_actor_index', [], Response::HTTP_SEE_OTHER);
+    }
+}
---
+++ src/Form/MovieType.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Movie;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class MovieType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('title')
+            ->add('releaseDate')
+            ->add('description')
+            ->add('actors')
+            ->add('category')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Movie::class,
+        ]);
+    }
+}
